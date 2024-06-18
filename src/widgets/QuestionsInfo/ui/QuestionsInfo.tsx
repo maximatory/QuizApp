dce6dd6da1d55f9262fb6817184df9629cca369b
Maksim Baranov <oooinfo@mail.ru>
2024-06-18 13:25:05 +0300

    add handleTimerEnd for redirecting at end of timer

--- src/widgets/QuestionsInfo/ui/QuestionsInfo.tsx
+++ src/widgets/QuestionsInfo/ui/QuestionsInfo.tsx
@@ -2,13 +2,16 @@ import { useAppSelector } from '@/app/appStore'
 import { TimerControl } from '@/features/timer/TimerControl'
 
 import styles from './styles.module.css'
+import { useNavigate } from 'react-router-dom'
 
 const QuestionsInfo = () => {
+  const navigate = useNavigate()
   const initialSeconds = useAppSelector((state) => state.questions.questionsData?.timerSeconds)
+  const handleTimerEnd = () => navigate('/fynally')
   return (
     <div className={styles.infoContainer}>
       <h1 className={styles.title}>Тестирование</h1>
-      {initialSeconds && <TimerControl initialSeconds={initialSeconds} />}
+      {initialSeconds && <TimerControl initialSeconds={initialSeconds} onTimerEnd={handleTimerEnd} />}
     </div>
   )
 }
